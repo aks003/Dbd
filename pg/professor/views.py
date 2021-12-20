59b3f64675d8d1a6b7b67b595a6601685697eb29
Abhiramkh <abiramsrivathsa.is192rvce.edu.in>
2021-12-20 08:42:05 +0530

    Tables

--- pg/professor/views.py
+++ pg/professor/views.py
@@ -8,53 +8,39 @@ from django import forms
 
 # Create your views here.
 def details(request):
-    if request.user.is_teacher:
+    if request.user.is_teacher :
         obj=professor_db.objects.get(email=request.user.email)
         obj1=student_db.objects.all().filter(prof_id=obj.prof_id)
-        print(obj1)
-    return render(request,'prof/prof_details.html',{'prof':obj,'stu':obj1})    
+        return render(request,'prof/prof_details.html',{'prof':obj,'stu':obj1})    
 def marks(request):
     if request.user.is_teacher:
-        obj2=rubrics_db.objects.all()
-        usn=0
-        obj=professor_db.objects.get(email=request.user.email)
-        prof=obj.prof_id
-        obj1=student_db.objects.all().filter(prof_id=obj.prof_id)
-        obj2= rubrics_db.objects.all()
         if request.method == 'GET':
             usn=request.GET.get('mark')
-            obj=professor_db.objects.get(email=request.user.email)
-            prof=obj.prof_id
-            obj1=student_db.objects.all().filter(prof_id=obj.prof_id)
-            obj2= rubrics_db.objects.all()
-            # obj=professor_db.objects.get(email=request.user.email)
-            # obj1=student_db.objects.all().filter(prof_id=obj.prof_id)
-            # obj2=evaluation_db.objects.all().filter(prof_id=obj.prof_id,)
-            # form1=[]
-            # usn1=[]
-            # prof=obj.prof_id
-            # for a in obj1:
-
-            #     fm=rubricsform(initial={'usn':a.usn})
-            #     fm.fields['usn'].widget=forms.HiddenInput()
-            #     fm.fields['prof'].widget=forms.HiddenInput()
-            #     usn1.append(a.usn)
-            #     form1.append(fm)
-            # list1=zip(form1,usn1)
-            obj4=rubrics_evaluation_db.objects.filter(usn=usn,prof=prof,rubrics=obj2.first()).first()
-            return render(request,'prof/marks1.html',{'prof':prof,'usn':usn,'obj2':obj2,'obj3':obj4})
-            print(usn)
+            prof=professor_db.objects.get(email=request.user.email)
+            prof_id=prof.prof_id
+            students=student_db.objects.all().filter(prof_id=prof_id)
+            rubrics= rubrics_db.objects.all()
+            phase=rubrics.first().phase_id
+            rubrics_eval=rubrics_evaluation_db.objects.all().filter(usn=usn)
+            print(rubrics_eval)
+            return render(request,'prof/marks1.html',{
+                'prof':prof,
+                'usn':usn,
+                'rubrics':rubrics,
+                'rubrics_eval':rubrics_eval,
+                'len':len(rubrics_eval),
+                'phase':phase
+                })
         else:
-            for a in obj2:
+            for a in rubrics_db.objects.all():
                 marks=request.POST[a.rname]
-                usn1=request.POST['usn']
-                obj2=student_db.objects.filter(usn=usn1).first()
-                obj3=professor_db.objects.get(email=request.user.email)
-                print(usn1)
-                obj=rubrics_evaluation_db(usn=obj2,prof=obj3,rubrics=a,r_marks_obtained=marks)
-                obj.save()
-                obj1=student_db.objects.all().filter(prof_id=obj.prof_id)
-            return render(request,'prof/prof_details.html',{'prof':obj3,'stu':obj1})  
+                usn=request.POST['usn']
+                student=student_db.objects.filter(usn=usn).first()
+                professor=professor_db.objects.get(email=request.user.email)
+                rubrics_eval=rubrics_evaluation_db(usn=student,prof=professor,rubrics=a,r_marks_obtained=marks)
+                rubrics_eval.save()
+                students=student_db.objects.all().filter(prof_id=rubrics_eval.prof_id)
+            return render(request,'prof/prof_details.html',{'prof':professor,'stu':students})  
 
 def stu_details(request):
     if request.user.is_teacher:
