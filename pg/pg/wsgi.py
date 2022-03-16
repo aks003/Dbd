1617ec3e40f9a1d5553f53a4dca003f24b0fe978
aks003 <akashshetty.is19@rvce.edu.in>
2022-03-16 21:41:42 +0530

    procfile change again

--- pg/pg/wsgi.py
+++ pg/pg/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pg.pg.settings')
 
 application = get_wsgi_application()
