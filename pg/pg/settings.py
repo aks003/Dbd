ac70ab54eaa1b55f4a083aed902c0b2ef949e206
aks003 <akashshetty.is19@rvce.edu.in>
2022-03-17 11:36:18 +0530

    whitenoise 1

--- pg/pg/settings.py
+++ pg/pg/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
